7010fbe6d2807a1dd04eb67da276545d964bda6a
是椿去湫来 <83126159+shiunke@users.noreply.github.com>
2021-10-27 21:03:47 +0800

    Update aiqicha.js

--- aiqicha.js
+++ aiqicha.js
@@ -1,8 +1,9 @@
 /*
-0 0,30 8 * * ? https://ghproxy.com/github.com/WindFgg/QuantumultX_Conf/blob/main/Scripts/aiqicha.js 爱企查日常任务
+0 0,30 8 * * ? https://ghproxy.com/github.com/shiunke/Scripts/blob/unke/aiqicha.js 爱企查任务
  爱企查下载地址：https://aiqicha.baidu.com/m/usercenter/inviteCode?uid=xlTM-TogKuTwvdzTq9EFdGvdxrmJpAANFgmd
 
- 手动抓取CK:https://passport.baidu.com/v2/?login 一个BDUSS=xxxxx;就行了
+ 手动抓取CK:https://passport.baidu.com/v2/?login
+ 一个BDUSS=xxxxx;就行了
  定时每天早上8点执行一次 8点半执行一次 账号多的请加大延迟 可能有一些任务跑不完 如:高级搜索 浏览监控日报 属正常现象 (百度抽风)
  多账号请用@分割Cookie  格式:ck1@ck2@ck2 青龙变量aqcCookies qx和v2p不可用
  批量查询任务暂时无法完成 
